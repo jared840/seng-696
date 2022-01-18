81a5475f5ea17cf898fc00dbbbcba79869b04dad
jared840 <81462044+jared840@users.noreply.github.com>
2022-01-18 03:35:06 -0700

    Added JMethod (#1)

--- Sample.java
+++ Sample.java
@@ -1,13 +1,20 @@
+
+
 public class Sample {
 
 	public static void main(String[] args) {
 
 	System.out.println("Hello from Jared");
 	System.out.println("Hello again");	
+	jmethod();
 }
 
 
+public static void jmethod() {
 
+System.out.println("J method");
+
+}
 
 
 
