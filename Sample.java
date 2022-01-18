0e1a6645a174e1774464e9f47dcc1a205f3084c0
jared840 <81462044+jared840@users.noreply.github.com>
2022-01-18 00:49:27 -0700

    Revert "Added mary() method"
    This reverts commit 1e381319f2176fce969d02a1632a00ef82dea368.

--- Sample.java
+++ Sample.java
@@ -4,13 +4,9 @@ public class Sample {
 
 	System.out.println("Hello from Jared");
 	System.out.println("Hello again");	
-	mary();
 }
 
-public static void mary() {
 
-	System.out.println("Hello from method!");
-}
 
 
 
